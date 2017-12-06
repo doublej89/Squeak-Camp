5a1dffdc4eba50b087ccde442a4851ac23cda017
copperware <jawad.jahangir.666@gmail.com>
2017-12-07 02:22:01 +0600

    added mongodb

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/sqeak_camp", {
+	useMongoClient: true
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,22 +15,66 @@ app.get("/", function(req, res) {
 	res.render("landing");
 });
 
+var campGroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+var Campground = mongoose.model("Campground", campGroundSchema);
+// Campground.create(
+// 	{ name: "Random Place", image: "https://farm7.staticflickr.com/6014/6015893151_044a2af184.jpg" },
+// 	function(err, campground){
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log("NEWLY CREATED CAMPGROUND: ");
+// 			console.log(campground);
+// 		}
+// 	}
+// );
+
+
 app.get("/campgrounds", function(req, res) {
-	res.render("campgrounds");
+	Campground.find({}, function(err, allCampgrounds) {
+		if (err) {
+			console.log(err);
+		} else{
+			res.render("index", {campgrounds: allCampgrounds});
+		}
+	});	
 });
 
 app.post("/campgrounds", function(req, res) {
 	var name = req.body.name;
 	var image = req.body.image;
+	var desc = req.body.description;
+	var newCampground = { name: name, image: image, description: desc };
 
-	var newCampground = { name: name, image: image };
-
-	res.redirect("/campgrounds");
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});	
 });
 
 app.get("/campgrounds/new", function(req, res) {
 	res.render("new.ejs");
 });
+
+app.get('/campgrounds/:id', function(req, res) {
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("show", {campground: foundCampground});
+		}
+	});
+	
+});
+
 app.listen(3000, function() {
 	console.log("SqueakCamp begins");
 });
