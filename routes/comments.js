26515a2f334116f47cf0c465b1ce1e842a2b6ea4
copperware <jawad.jahangir.666@gmail.com>
2017-12-30 02:29:27 +0600

    Editing or deleting comments now require authorization

--- routes/comments.js
+++ routes/comments.js
@@ -35,7 +35,7 @@ router.post("/", isLoggedIn, function(req, res) {
 	});
 });
 
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res) {
 	Comment.findById(req.params.comment_id, function(err, foundComment) {
 		if (err) {
 			res.redirect("back");
@@ -45,8 +45,18 @@ router.get("/:comment_id/edit", function(req, res) {
 	});	
 });
 
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership, function(req, res) {
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, foundComment) {
+		if (err) {
+			res.redirect("back");
+		} else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+router.delete("/:comment_id", checkCommentOwnership, function(req, res) {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
 		if (err) {
 			res.redirect("back");
 		} else {
@@ -60,6 +70,24 @@ function isLoggedIn(req, res, next) {
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+	if (req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id, function(err, foundComment) {
+			if (err) {
+				res.redirect("back");
+			} else {
+				if (foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}				
+			}
+		});
+	} else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
