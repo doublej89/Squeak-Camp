f02811a04dabf2cb06f33f53f674e6c091f896b5
Jawad Jahangir <jawad.jahangir.666@gmail.com>
2018-05-29 10:23:24 +0600

    env variables established

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
@@ -16,11 +16,11 @@ var campgroundRoutes = require("./routes/campgrounds");
 var commentRoutes = require("./routes/comments");
 var indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/sqeak_camp", {
-	useMongoClient: true
+mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
@@ -28,29 +28,31 @@ app.use(flash());
 app.set("view engine", "ejs");
 
 //seedDB();
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
-app.use(require("express-session")({
-	secret: "Kuttar Bachcha is a stupid bitch!",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Kuttar Bachcha is a stupid bitch!",
+    resave: false,
+    saveUninitialized: false
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localPass(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	next();
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function() {
-	console.log("SqueakCamp begins");
+app.listen(process.env.PORT || 3000, function() {
+  console.log("SqueakCamp begins");
 });
